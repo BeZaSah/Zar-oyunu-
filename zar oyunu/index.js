6463a85d04fda99028365c4bd9a0199da3f66a54
Tobi <91742164+BeZaSah@users.noreply.github.com>
2024-08-31 01:52:16 +0300

    Zar Oyunu

---
+++ zar oyunu/index.js
@@ -0,0 +1,32 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //random sayı oluşturur 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png-dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png-images/dice6.png
+
+var image1 = document.querySelectorAll("img") [0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; 
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//1. oyuncu kazanırsa
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🥇1. Oyuncu Kazandı!"
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "🥇2. Oyuncu Kazandı!"
+}
+else {
+    document.querySelector("h1").innerHTML = "🏳Berabere!🏳"
+}
+
+
+
+
