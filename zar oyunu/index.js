0091a70e3d96c33d8642a09ba3a0786ea54bd2ab
Tobi <91742164+BeZaSah@users.noreply.github.com>
2024-10-13 23:29:48 +0300

    Zar oyunu

--- zar oyunu/index.js
+++ zar oyunu/index.js
@@ -1,32 +1,46 @@
+let rollCount = 0; // Zar döndürme sayacını başlat
+let interval; // Interval değişkeni tanımlanıyor
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //random sayı oluşturur 1-6
+function startRolling() {
+  // Eğer interval(bir işlemin belirli aralıklarla tekrarlanması) zaten ayarlanmışsa, temizle
+  if (interval) {
+    clearInterval(interval);
+  }
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png-dice6.png
+  rollCount = 0; // Sayaç sıfırla
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png-images/dice6.png
+  // Zar döndürme işlemini başlat
+  interval = setInterval(function () {
+    // 1. Zar atışı için rastgele sayı üret
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
+    let randomImageSource = "images/" + randomDiceImage; // images/dice1.png - dice6.png
 
-var image1 = document.querySelectorAll("img") [0];
+    // 2. Zar atışını ekranda göster
+    let image1 = document.querySelector(".img1");
+    image1.setAttribute("src", randomImageSource);
 
-image1.setAttribute("src", randomImageSource);
+    // 3. İkinci zar için rastgele sayı üret
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
+    // 4. İkinci zar atışını ekranda göster
+    document.querySelector(".img2").setAttribute("src", randomImageSource2);
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+    rollCount++; // Her atışta sayaç artar
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; 
+    // 5 kez döndükten sonra durdur ve sonucu göster
+    if (rollCount >= 5) {
+      clearInterval(interval); // Zar döndürme işlemini durdur
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-//1. oyuncu kazanırsa
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🥇1. Oyuncu Kazandı!"
+      // Kazananı belirle ve sonucu göster
+      if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🥇1. Oyuncu Kazandı!";
+      } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "🥇2. Oyuncu Kazandı!";
+      } else {
+        document.querySelector("h1").innerHTML = "🏳Berabere!🏳";
+      }
+    }
+  }, 500); // Her yarım saniyede bir zar döndür
 }
-else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "🥇2. Oyuncu Kazandı!"
-}
-else {
-    document.querySelector("h1").innerHTML = "🏳Berabere!🏳"
-}
-
-
-
-
